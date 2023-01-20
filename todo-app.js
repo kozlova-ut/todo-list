753dc99a77f393c7e17dea5be474fc15206ea9a0
Юлия Козлова <75185563+kozlova-ut@users.noreply.github.com>
2023-01-20 14:59:02 +0300

    fixed item deletion

--- todo-app.js
+++ todo-app.js
@@ -44,7 +44,7 @@
         let deleteButton = document.createElement('button');
 
         item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-        
+
         if (typeof(task) === 'object') {
             item.textContent = task.name;
 
@@ -84,14 +84,14 @@
 
         todoItem.doneButton.addEventListener('click', function() {
             todoItem.item.classList.toggle('list-group-item-success');
-            
+
             let indexValue;
-            let itemValue = todoItem.item.childNodes[0].data;  
-            
+            let itemValue = todoItem.item.childNodes[0].data;
+
             for (let a of array) {
                 if (a.name == itemValue) {
-                    indexValue = array.indexOf(a);              
-                };            
+                    indexValue = array.indexOf(a);
+                };
             };
 
             let status = array[indexValue].done;
@@ -110,23 +110,23 @@
         todoItem.deleteButton.addEventListener('click', function() {
             if (confirm('Вы уверены?')) {
                 todoItem.item.remove();
+
+				let indexValue = undefined;
+				let itemValue = todoItem.item.childNodes[0].data;
+
+				for (let a of array) {
+					if (a.name == itemValue) {
+						indexValue = array.indexOf(a);
+					};
+				};
+
+				array.splice(indexValue, 1);
+
+				localStorage.setItem(key, JSON.stringify(array));
             };
-
-            let indexValue = undefined;
-            let itemValue = todoItem.item.childNodes[0].data;  
-
-            for (let a of array) {
-                if (a.name == itemValue) {
-                    indexValue = array.indexOf(a);              
-                };            
-            };
-
-            array.splice(indexValue, 1);
-
-            localStorage.setItem(key, JSON.stringify(array));
         });
     };
- 
+
     function createTodoApp(container, title = 'Список дел', array, key) {
         if (JSON.parse(localStorage.getItem(key)) != null) {
             array = JSON.parse(localStorage.getItem(key));
